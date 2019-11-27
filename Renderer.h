2b3c9d95fc5d61b83a1049b8225c21e0e003aeb5
Taehwan Kim <49132911+taehwan642@users.noreply.github.com>
2019-11-27 23:40:04 +0900

    Updated

--- Renderer.h
+++ Renderer.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "Type.h"
+#include "Node.h"
 class Renderer : public SingleTon<Renderer>
 {
-
+private:
+	list<Node*> _rendertargets;
+	static bool Comp(Node* a, Node* b);
+public:
+	void AddRenderTarget(Node* node);
+	void RemoveRenderTarget(Node* node);
+	void Render();
 };
 
--- Scene.h
+++ Scene.h
@@ -1,6 +1,9 @@
 #pragma once
 class Scene
 {
-
+public:
+	virtual void Init() {};
+	virtual void Update() {};
+	virtual void Exit() {};
 };
 
